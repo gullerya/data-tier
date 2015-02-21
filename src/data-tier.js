0c3ad4b28cd593143e7561df3e07489f57fe0f28
Yuri Guller <gullerya@gmail.com>
2015-02-22 01:58:07 +0200

    wip

--- src/data-tier.js
+++ src/data-tier.js
@@ -357,7 +357,7 @@
 									collectViews(this.contentDocument);
 									initDomObserver(this.contentDocument);
 								});
-								change.addedNodes[i].contentWindow.addEventListener('unload', function () {
+								change.addedNodes[i].contentWindow.addEventListener('beforeunload', function () {
 									discardViews(this.document);
 								});
 							} else {
