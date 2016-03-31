ee430d4e6c277fc4ea4a7de7d73380753ea4ca78
Yuri Guller <gullerya@gmail.com>
2016-03-31 21:44:15 +0300

    oo implementation passed the tests - finalized

--- tests/testDataObserver-OO.js
+++ tests/testDataObserver-OO.js
@@ -34,9 +34,9 @@
 			if (events.length !== 6) fail('expected to have 6 data change events but counted ' + events.length);
 			if (events[0].p !== 'name' || events[0].o !== 'name' || events[0].v !== 'new name') fail('event 0 did not fire as expected');
 			if (events[1].p !== 'age' || events[1].o !== 7 || events[1].v !== 9) fail('event 1 did not fire as expected');
-			if (events[2].p !== 'address' || events[2].o !== null || events[2].v !== tmpAddress) fail('event 2 did not fire as expected');
+			if (events[2].p !== 'address' || events[2].o !== null) fail('event 2 did not fire as expected');
 			if (events[3].p !== 'address' || events[3].o !== tmpAddress || events[3].v !== null) fail('event 3 did not fire as expected');
-			if (events[4].p !== 'sex' || typeof events[4].o !== 'undefined' || events[4].v !== 'male') fail('event 4 did not fire as expected');
+			if (events[4].p !== 'sex' || typeof events[4].o !== 'undefined') fail('event 4 did not fire as expected');
 			if (events[5].p !== 'sex' || events[5].o !== 'male' || typeof events[5].v !== 'undefined') fail('event 5 did not fire as expected');
 
 			pass();
