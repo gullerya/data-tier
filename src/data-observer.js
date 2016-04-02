75a260b5067e4d11951ae04b201eeee78a54b21b
Yuri Guller <gullerya@gmail.com>
2016-04-02 09:37:08 +0300

    flattener - optimizations

--- src/data-observer.js
+++ src/data-observer.js
@@ -3,36 +3,34 @@
 
 	var pathsMap = new WeakMap();
 
-	function flattenObject(graph) {
-		let result = {};
-
-		if (typeof graph !== 'object') { throw new Error('illegal graph argument, object expected'); }
-
-		function iterate(graph, path) {
-			var tmp, pVal;
-			if (Array.isArray(graph)) {
-				tmp = (path ? path : '') + '[';
-				graph.forEach(function (itm, idx) {
-					if (itm && typeof itm === 'object') {
-						iterate(itm, [tmp, idx, ']'].join(''));
-					} else {
-						result[[tmp, idx, ']'].join('')] = itm;
-					}
-				});
-			} else {
-				tmp = (path ? path + '.' : '');
-				Reflect.ownKeys(graph).forEach(function (pKey) {
-					pVal = graph[pKey];
-					if (pVal && typeof pVal === 'object') {
-						iterate(pVal, tmp + pKey);
-					} else {
-						result[tmp + pKey] = pVal;
-					}
-				});
-			}
+	function iterate(graph, result, path) {
+		var tmp, pVal;
+		if (Array.isArray(graph)) {
+			tmp = path + '[';
+			graph.forEach(function (itm, idx) {
+				if (itm && typeof itm === 'object') {
+					iterate(itm, result, [tmp, idx, ']'].join(''));
+				} else {
+					result[[tmp, idx, ']'].join('')] = itm;
+				}
+			});
+		} else {
+			tmp = (path ? path + '.' : '');
+			Reflect.ownKeys(graph).forEach(function (pKey) {
+				pVal = graph[pKey];
+				if (pVal && typeof pVal === 'object') {
+					iterate(pVal, result, tmp + pKey);
+				} else {
+					result[tmp + pKey] = pVal;
+				}
+			});
 		}
-		if (graph) { iterate(graph); }
+	}
 
+	function flattenObject(graph) {
+		var result = {};
+		if (typeof graph !== 'object') { throw new Error('illegal graph argument, object expected'); }
+		if (graph) { iterate(graph, result, ''); }
 		return result;
 	}
 
