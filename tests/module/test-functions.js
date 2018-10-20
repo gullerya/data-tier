cee61bbac832c6ede4ba4e76e51b1a1956d63cdf
Yuri Guller <gullerya@gmail.com>
2018-10-20 23:29:40 +0300

    tech: added a test to verify functional binding works as expected

---
+++ tests/module/test-functions.js
@@ -0,0 +1,34 @@
+import * as DataTier from '../../dist/module/data-tier.js';
+
+let suite = Utils.JustTest.createSuite({name: 'Testing functions dynamic binding'}),
+	clickCounter = 0,
+	user = {
+		name: 'click me',
+		clickHandler: function(e) {
+			clickCounter++;
+			DataTier.ties.get('functionalTest').model.name = 'click again';
+			DataTier.ties.get('functionalTest').model.clickHandler = function() {
+				clickCounter += 2;
+			}
+		}
+	};
+
+DataTier.ties.create('functionalTest', user);
+
+suite.addTest({name: 'bind onclick logic'}, (pass, fail) => {
+	let d = document.createElement('div');
+	d.dataset.tie = 'functionalTest:clickHandler => onclick, functionalTest:name => textContent';
+	document.body.appendChild(d);
+
+	setTimeout(() => {
+		d.click();
+		if (d.textContent !== 'click again' || clickCounter !== 1) fail('expected to register result of a first click');
+
+		d.click();
+		if (clickCounter !== 3) fail('expected to register result of a second click');
+
+		pass();
+	}, 0);
+});
+
+suite.run();
