9570037c6effdf1dcfd497db25e7a0408294ef55
Yuri Guller <gullerya@gmail.com>
2018-10-27 13:47:52 +0300

    issue #13: adding test to verify the described use-case

---
+++ tests/module/test-array-model-static-binding.js
@@ -0,0 +1,41 @@
+import * as DataTier from '../../dist/module/data-tier.js';
+import Observable from '../../dist/module/object-observer.min.js';
+
+let suite = Utils.JustTest.createSuite({name: 'Testing array changes (static binding)'}),
+	ordersTie = DataTier.ties.create('ordersASB'),
+	element = document.createElement('div');
+
+suite.addTest({name: 'array manipulation flow'}, async (pass, fail) => {
+
+	//	add element with static binding on element 2
+	element.dataset.tie = 'ordersASB:2 => textContent';
+	document.body.appendChild(element);
+	await new Promise(res => setTimeout(res, 0));
+	if (element.textContent !== '') fail('expected textContent to be empty, found ' + element.textContent);
+
+	//	add array having element 2
+	ordersTie.model = ['0', '1', '2', '3'];
+	await new Promise(res => setTimeout(res, 0));
+	if (element.textContent !== '2') fail('expected textContent to be "2", found ' + element.textContent);
+
+	//	unshift
+	ordersTie.model.unshift('primordial');
+	await new Promise(res => setTimeout(res, 0));
+	if (element.textContent !== '1') fail('expected textContent to be "1", found ' + element.textContent);
+
+	//	shift
+	ordersTie.model.shift();
+	await new Promise(res => setTimeout(res, 0));
+	if (element.textContent !== '2') fail('expected textContent to be "2", found ' + element.textContent);
+	ordersTie.model.shift();
+	await new Promise(res => setTimeout(res, 0));
+	if (element.textContent !== '3') fail('expected textContent to be "3", found ' + element.textContent);
+
+	//	push
+
+	//	pop
+
+	pass();
+});
+
+suite.run();
