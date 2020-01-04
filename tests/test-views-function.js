50f5325ba8f7a635a0ee836a26f7936ae2e2d73c
gullerya <gullerya@gmail.com>
2020-01-04 23:54:58 +0200

    issue #36: added more tests

--- tests/test-views-function.js
+++ tests/test-views-function.js
@@ -3,6 +3,8 @@ import * as DataTier from '../dist/data-tier.js';
 
 const suite = createSuite({ name: 'Testing views changes - functional tying' });
 
+//	single argument
+//
 suite.runTest({ name: 'adding root model single to view - model first' }, async test => {
 	const tieName = test.getRandom(8);
 	const model = DataTier.ties.create(tieName, { test: 'test' });
@@ -69,4 +71,49 @@ suite.runTest({ name: 'adding deep model single to view - view first' }, async t
 	test.assertEqual(1, calls.length);
 	test.assertEqual('test', calls[0][0]);
 	test.assertTrue(typeof calls[0][1] === 'object');
+});
+
+//	multiple arguments
+//
+suite.runTest({ name: 'adding mixed models multi to view - model first' }, async test => {
+	const tieNameA = test.getRandom(8);
+	const tieNameB = test.getRandom(8);
+	const model = DataTier.ties.create(tieNameA, { deep: { test: 'test' } });
+	DataTier.ties.create(tieNameB, { deep: { test: 'test' } });
+
+	const calls = [];
+	const newEl = document.createElement('div');
+	newEl.f = function () { calls.push(arguments) };
+	newEl.dataset.tie = 'f(' + tieNameA + ', ' + tieNameB + ':deep.test)';
+	document.body.appendChild(newEl);
+
+	await test.waitNextMicrotask();
+
+	test.assertEqual(1, calls.length);
+	test.assertEqual(model, calls[0][0]);
+	test.assertEqual('test', calls[0][1]);
+	test.assertEqual(null, calls[0][2]);
+});
+
+suite.runTest({ name: 'adding mixed models multi to view - view first' }, async test => {
+	const tieNameA = test.getRandom(8);
+	const tieNameB = test.getRandom(8);
+
+	const calls = [];
+	const newEl = document.createElement('div');
+	newEl.f = function () { calls.push(arguments) };
+	newEl.dataset.tie = 'f(' + tieNameB + ':deep.test, ' + tieNameA + ')';
+	document.body.appendChild(newEl);
+
+	await test.waitNextMicrotask();
+
+	const model = DataTier.ties.create(tieNameA, { deep: { test: 'test' } });
+	DataTier.ties.create(tieNameB, { deep: { test: 'test' } });
+	test.assertEqual(2, calls.length);
+	test.assertEqual(undefined, calls[0][0]);
+	test.assertEqual(model, calls[0][1]);
+	test.assertTrue(typeof calls[0][2] === 'object');
+	test.assertEqual('test', calls[1][0]);
+	test.assertEqual(model, calls[1][1]);
+	test.assertTrue(typeof calls[1][2] === 'object');
 });
